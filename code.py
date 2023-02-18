d44ee7ca4b51075ba84c9129675c5bd7dbbfe632
Jkollie <116442162+Jkollie1@users.noreply.github.com>
2023-02-18 13:13:23 +0000

    Add files via upload

---
+++ code.py
@@ -0,0 +1,36 @@
+greetings_message = "From nova web development python group project"
+print(greetings_message)
+print("the below program will accept information from a user and than welcome them\n")
+
+def user_info(name,about):
+      print('welcome to", greetings_message')
+      print("hello", name)
+      print("you are", name)
+
+
+
+name = input('what is your name?\n')
+print(name)
+print('hello, you are welcome')
+
+# Choosing mnemonic variable names
+hours = 35.0
+rate = 12.50
+pay = hours * rate
+print(pay)
+
+# values an types
+width = 17
+(width//2)
+type('width//2')
+
+(width/2.0)
+type('width/2.0')
+
+height = 12.0
+(height/3)
+type('height/3')
+
+(1 + 2 * 5)
+type('1 + 2 * 5')
+
