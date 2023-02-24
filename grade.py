ab88e3367f7f2c9b4dc009299fc222e336259969
Jkollie <116442162+Jkollie1@users.noreply.github.com>
2023-02-24 22:30:19 +0000

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,13 @@
+def my_score(First, second):
+
+
+Result = First/Second
+       if  result < 1.0:
+       print('your answer is"A)
+       
+       else:
+       print("your answer is out of range")
+       
+First =int(input("Enter First score")
+second =int(input("Enter Second Score")
+my_score(First, Second)
