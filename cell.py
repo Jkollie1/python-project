3b42d018d604a0a25cbda0db11eb95f618683d69
Jkollie <116442162+Jkollie1@users.noreply.github.com>
2023-03-03 11:16:59 +0000

    Add files via upload

---
+++ cell.py
@@ -0,0 +1,70 @@
+def print_twice(bruce):
+    print(bruce)
+    print(bruce)
+
+print_twice('Janet')
+print_twice('100')
+
+
+import  math
+print_twice(math.pi)
+
+print_twice('Janet '*4)
+
+print_twice(math.cos(math.pi))
+
+
+def addtwo(j, k):
+    added = j + k
+    return added
+
+x = addtwo(7, 9)
+print(x)
+
+spencer = 'freena, the half a bee.'
+print_twice(spencer)
+
+
+def Spencer():
+    print("zap")
+    
+def Janet():
+    print("ABC")
+    
+    
+Janet()
+Spencer()
+Janet()
+print(x)
+
+hours = 96.25 *  40 * 1.5 / 10
+rate = 102.5
+pay = hours - rate
+print (pay)
+
+
+inp = input('Enter Fahrenheit Temperature:')
+try:
+    fahr = float(inp)
+    cel = (fahr - 32.0) * 5.0 / 9.0
+    print(cel)
+except:
+    print('Please enter a number')
+    
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+    
+    repeat_lyrics()
+
+    
+def print_lyrcis():
+    print("I'm having a great time studying python, and i'm okay.")
+    print('I sleep all night, and I work all day.')
+    
+
+
+
+    
+    
